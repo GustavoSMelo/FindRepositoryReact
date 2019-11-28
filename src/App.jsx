053d80cd356736332d50f89352b8d44fe94d7cba
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-11-27 22:06:19 -0300

    Pagina jsx principal

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes/routes';
+import GlobalStyles from './styles/styles';
+
+function App() {
+    return (
+        <>
+            <Routes></Routes>
+            <GlobalStyles></GlobalStyles>
+        </>
+    );
+}
+
+export default App;
